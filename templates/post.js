2a18e4137a48d5f31d680d9a32a6634c985ad874
Linghao Zhang <linghaozh@gmail.com>
2019-05-16 16:10:31 -0700

    add meta tags

--- templates/post.js
+++ templates/post.js
@@ -6,7 +6,11 @@ import theme from '../theme'
 export default meta => ({ children }) => <MDXProvider components={theme.components}><>
   <Head>
     <title>{meta.title}</title>
+    <meta property="og:site_name" content="Linghao's Blog" />
     <meta property="og:description" content={meta.description} />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content={meta.title} />
+    <meta name="twitter:description" content={meta.description} />
   </Head>
   <theme.Container itemScope itemType='http://schema.org/blogPost'>
     <theme.components.h1 itemType='headline'>{meta.title}</theme.components.h1>
