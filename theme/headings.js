d9201f8767764847d6305dea2397f2e878f2d585
Linghao Zhang <linghaozh@gmail.com>
2019-05-16 17:02:33 -0700

    add H4 & tweak scale

--- theme/headings.js
+++ theme/headings.js
@@ -3,7 +3,7 @@ const H1 = ({ children, id, ...props }) => <h1 id={id} {...props}>
   <style jsx>{`
     h1 {
       font-size: 1.5em;
-      font-weight: 600;
+      font-weight: 800;
     }
   `}</style>
 </h1>
@@ -13,8 +13,8 @@ const H2 = ({ children, id }) => <h2 id={id}>
   <style jsx>{`
     h2 {
       margin-top: 2.5rem;
-      font-size: 1.3em;
-      font-weight: 600;
+      font-size: 1.333em;
+      font-weight: 700;
     }
     a {
       color: inherit;
@@ -27,9 +27,9 @@ const H3 = ({ children, id }) => <h3 id={id}>
   <a href={'#' + id}>{children}</a>
   <style jsx>{`
     h3 {
-      font-size: 1.1em;
-      margin-top: 2.5rem;
-      font-weight: 400;
+      margin-top: 2.0rem;
+      font-size: 1.185em;
+      font-weight: 600;
     }
     a {
       color: inherit;
@@ -38,8 +38,24 @@ const H3 = ({ children, id }) => <h3 id={id}>
   `}</style>
 </h3>
 
+const H4 = ({ children, id }) => <h4 id={id}>
+  <a href={'#' + id}>{children}</a>
+  <style jsx>{`
+    h4 {
+      margin-top: 1.5rem;
+      font-size: 1.053em;
+      font-weight: 600;
+    }
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+  `}</style>
+</h4>
+
 export default {
   h1: H1, 
   h2: H2,
-  h3: H3
+  h3: H3,
+  h4: H4
 }
