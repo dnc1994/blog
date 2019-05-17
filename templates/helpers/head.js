cea12fa1ece09e29731ffdd97c7fc3190a554f3f
Linghao Zhang <linghaozh@gmail.com>
2019-05-16 17:24:01 -0700

    add favicon

--- templates/helpers/head.js
+++ templates/helpers/head.js
@@ -6,4 +6,10 @@ export default ({ children }) => <Head>
   <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
   <meta property="og:site_name" content="Linghao's Blog" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+  <link rel="manifest" href="/static/site.webmanifest" />
+  <meta name="msapplication-TileColor" content="#da532c" />
+  <meta name="theme-color" content="#ffffff" />
 </Head>
