956a7b3c7451aa33b60d476502529b3ff4d7d43d
Shu Ding <ds303077135@gmail.com>
2020-06-08 12:34:48 +0800

    Update disqus.js

--- theme/disqus.js
+++ theme/disqus.js
@@ -2,34 +2,30 @@ import { useEffect } from 'react'
 
 export default function Disqus () {
   useEffect(() => {
+    let node
     try {
       // load disqus when the component is mounted
-      window['DISQUS'].host._loadEmbed()
+      window.disqus_config = function () {
+        this.page.url = window.location.href  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = window.location.pathname // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+      }
+      const d = document, s = d.createElement('script')
+      s.src = 'https://linghao-io.disqus.com/embed.js'
+      s.setAttribute('data-timestamp', +new Date())
+      (d.head || d.body).appendChild(s)
+      node = s
     } catch (err) {
       // ignore
     }
+    
+    return () => {
+      if (node) node.remove()
+    }
   }, [])
 
-  return <>
+  return (
     <div id="disqus_thread" style={{
       marginBottom: 100
     }}/>
-    <script dangerouslySetInnerHTML={{
-      __html: `
-        var disqus_config = function () {
-          this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
-          this.page.identifier = window.location.pathname; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-        };
-        (function() { // DON'T EDIT BELOW THIS LINE
-          var d = document, s = d.createElement('script');
-          s.src = 'https://linghao-io.disqus.com/embed.js';
-          s.setAttribute('data-timestamp', +new Date());
-          (d.head || d.body).appendChild(s);
-        })();
-      `
-    }}/>
-    <noscript>
-      Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>
-    </noscript>
-  </>
+  )
 }
