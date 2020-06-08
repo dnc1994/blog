7695e7ba7f93c7fc5b11323a6d833702d2a35fa4
Shu Ding <ds303077135@gmail.com>
2020-06-08 14:43:43 +0800

    Update disqus.js

--- theme/disqus.js
+++ theme/disqus.js
@@ -2,24 +2,18 @@ import { useEffect } from 'react'
 
 export default function Disqus () {
   useEffect(() => {
-    let node
     try {
       // load disqus when the component is mounted
-      window.disqus_config = function () {
-        this.page.url = window.location.href  // Replace PAGE_URL with your page's canonical URL variable
-        this.page.identifier = window.location.pathname // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+      if (window['DISQUS']) {
+        window['DISQUS'].host._loadEmbed()
+      } else {
+        const d = document, s = d.createElement('script')
+        s.src = 'https://linghao-io.disqus.com/embed.js'
+        s.setAttribute('data-timestamp', '' + Date.now())
+        ;(d.head || d.body).appendChild(s)
       }
-      const d = document, s = d.createElement('script')
-      s.src = 'https://linghao-io.disqus.com/embed.js'
-      s.setAttribute('data-timestamp', +new Date())
-      (d.head || d.body).appendChild(s)
-      node = s
     } catch (err) {
       // ignore
-    }
-    
-    return () => {
-      if (node) node.remove()
     }
   }, [])
 
