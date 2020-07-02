4fe5e97da055d03e925711040b805ed7885a0559
Linghao Zhang <hi@linghao.io>
2020-07-02 01:06:54 -0700

    update about & adjust font

--- theme/container.js
+++ theme/container.js
@@ -18,7 +18,7 @@ export default ({ children, ...props }) => (
         word-break: break-word;
         word-wrap: break-word;
         hyphens: auto;
-        font-family: 'Merriweather', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'Microsoft Yahei', sans-serif;
+        font-family: 'Roboto', 'Helvetica Neue', -apple-system, BlinkMacSystemFont, Helvetica, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Microsoft Yahei', sans-serif;
         font-feature-settings: 'liga' 1, 'dlig' 1, 'lnum' 1, 'ordn' 1, 'halt' 1, 'kern' 1, 'kern', 'mgrk';
         font-kerning: auto;
         font-variant: normal;
@@ -29,7 +29,7 @@ export default ({ children, ...props }) => (
         background-color: white;
       }
       
-      @import url('https://fonts.googleapis.com/css?family=Merriweather:400,400i,600,600i&font-display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 
       @media all and (max-width: 640px) {
         div {
