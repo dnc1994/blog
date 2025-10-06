2bb89a1b56642fa91c56f1a6769605223da84613
chenxizh <foreseazhang@gmail.com>
2025-10-05 19:40:11 -0700

    feat: setup ci & cf page deployment

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,13 @@ import { NextConfig } from 'next'
 
 export default withMDX()({
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  output: 'export',
+  distDir: 'dist',
   transpilePackages: ['shiki'],
   images: {
     contentDispositionType: 'inline',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true,
   },
 } satisfies NextConfig)
 
