6f9abb8edb0544fe42a7aec4c685004279f72730
Linghao Zhang <linghaozh@gmail.com>
2025-11-18 23:37:07 -0800

    test GA

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -8,6 +8,7 @@ const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID
 
 const pageview = (url: string) => {
   if (typeof window.gtag !== 'undefined') {
+    // @ts-ignore
     window.gtag('config', GA_MEASUREMENT_ID as string, {
       page_path: url,
     })
@@ -31,6 +32,20 @@ export const GoogleAnalytics = () => {
 
   return (
     <>
+      {/* Temporary debug indicator */}
+      <div
+        style={{
+          position: 'fixed',
+          bottom: '10px',
+          right: '10px',
+          width: '10px',
+          height: '10px',
+          backgroundColor: 'red',
+          borderRadius: '50%',
+          zIndex: 9999,
+        }}
+        title={`GA Active: ${GA_MEASUREMENT_ID}`}
+      />
       <Script
         strategy='afterInteractive'
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
@@ -42,6 +57,7 @@ export const GoogleAnalytics = () => {
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
+            // @ts-ignore
             gtag('js', new Date());
             gtag('config', '${GA_MEASUREMENT_ID}', {
               page_path: window.location.pathname,
